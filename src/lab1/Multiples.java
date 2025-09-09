7408e4dde461e8d1ff11e1f5f22029ca916447e4
Evelyn <evelyncjin@gmail.com>
2025-09-08 21:55:38 -0400

    Update project: add Multiples.java and fix test files

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int i = 1;
+        int tally = 0;
+
+        while (i < n) {
+            if (i % a == 0 || i % b == 0) {
+                tally++;
+            }
+            i++;
+        }
+        System.out.println(tally);
+        return tally;
+    }
+}
